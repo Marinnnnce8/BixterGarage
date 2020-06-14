eb28d01f2f20c1e4d875be9562ce7fa76862a22e
Marina Tatic <tatic.marina8@gmail.com>
2020-06-14 09:59:42 +0200

    Menu fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
-	entry: {},
-	output: {
-		path: path.join(__dirname, './dist/js/'),
-		filename: '[name].min.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: path.join(__dirname),
-				loader: 'babel-loader'
-			}
-		]
-	}
-};
+// module.exports = {
+// 	entry: {},
+// 	output: {
+// 		path: path.join(__dirname, './dist/js/'),
+// 		filename: '[name].min.js'
+// 	},
+// 	module: {
+// 		loaders: [
+// 			{
+// 				test: path.join(__dirname),
+// 				loader: 'babel-loader'
+// 			}
+// 		]
+// 	}
+// };
